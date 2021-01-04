6342000ab00c9729da10167e1f33798ec7e1eb58
canberkdurmus <canberkdurmus@gmail.com>
2021-01-04 17:22:40 +0300

    Remove '/' from path

--- BrowserRequest.py
+++ BrowserRequest.py
@@ -6,14 +6,10 @@ class BrowserRequest:
         # First line takes the form of
         # GET /file/path/ HTTP/1.1
         self.method, self.path, self.http_version = lines.pop(0).split(" ")
+        self.path = self.path.lstrip('/')
         self.info = {k: v for k, v in (line.split(': ') for line in lines)}
 
     def __repr__(self) -> str:
         return "<BrowserRequest {method} {path} {http_version}>".format(method=self.method, path=self.path,
                                                                         http_version=self.http_version)
 
-    def __getattr__(self, name: str):
-        try:
-            return self.info["-".join([n.capitalize() for n in name.split('_')])]
-        except IndexError:
-            raise AttributeError(name)
