e9eab949ce2786218ae78f229330f546e7078dd3
canberkdurmus <canberkdurmus@gmail.com>
2021-01-04 17:21:17 +0300

    Class simplified

--- http_server.py
+++ http_server.py
@@ -1,27 +1,16 @@
 import os
-
+import argparse
 from ServerSocket import ServerSocket
 
 
-class SimpleServer:
-
-    def __init__(self, port=80, homedir=os.path.curdir, page404=None):
-        """
-        Initialize a webserver
-
-        port    -- port to server requests from
-        homedir -- path to serve files out of
-        page404 -- optional path to HTML file for 404 errors
-        """
+class Server:
+    def __init__(self, port=80, homedir=os.path.curdir):
         self.socket = ServerSocket(port=port)
         self.homedir = os.path.abspath(homedir)
-        if page404:
-            with open(page404) as f:
-                self.response_404 = f.read()
 
     def serve(self):
+        print('Listening {}:{} in {}'.format(self.socket.host, self.socket.port, self.homedir))
         self.socket.open()
-        self.socket.log('Opening socket conn {}:{} in {}'.format(self.socket.host, self.socket.port, self.homedir))
         self.socket.listen()
 
     def stop(self):
@@ -29,12 +18,12 @@ class SimpleServer:
 
 
 if __name__ == "__main__":
-    import argparse
 
-    parser = argparse.ArgumentParser(description='Runs a simple Python server. Not for production')
-    parser.add_argument('port', type=int, help='port to run the server on')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('port', type=int)
     args = parser.parse_args()
-    server = SimpleServer(args.port)
+
+    server = Server(args.port)
     try:
         server.serve()
     finally:
