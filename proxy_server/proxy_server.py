a7909638309d538b681e7a0374063be569786c69
canberkdurmus <canberkdurmus@gmail.com>
2021-01-06 16:56:27 +0300

    Initial message for testlocal info

--- proxy_server/proxy_server.py
+++ proxy_server/proxy_server.py
@@ -9,7 +9,8 @@ class Server:
         self.homedir = os.path.abspath(homedir)
 
     def serve(self):
-        print('Listening {}:{} in {}\n'.format(self.socket.host, self.socket.port, self.homedir))
+        print('Proxy Server is Listening {}:{} in {}'.format(self.socket.host, self.socket.port, self.homedir))
+        print("Use 'testlocal' instead of 'localhost' if system overrides proxy settings for localhost \n")
         self.socket.open()
         self.socket.listen()
 
